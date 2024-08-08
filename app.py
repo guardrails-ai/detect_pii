2d55953324135191217ddcb1a32e837c1ab54a4d
Aarav Navani <38411399+aaravnavani@users.noreply.github.com>
2024-08-08 15:27:35 -0700

    fix bug when size of entities is 1

--- app.py
+++ app.py
@@ -11,8 +11,11 @@ class InferlessPythonModel:
         self.pii_anonymizer = AnonymizerEngine()
 
     def infer(self, inputs: Dict[str, Any]):
+        entities_to_use = inputs["entities"]
+        if isinstance(entities_to_use, str):
+            entities_to_use = [entities_to_use]
         results = self.pii_analyzer.analyze(
-            text=inputs["text"], entities=inputs["entities"], language="en"
+            text=inputs["text"], entities=entities_to_use, language="en"
         )
         results = cast(List[Any], results)
         anonymized_text = self.pii_anonymizer.anonymize(
